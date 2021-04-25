ac657f4a381f8ecd29dd8f9f70195321dcd43770
fabbioAmaral <fabioamaral.93@gmail.com>
2021-04-25 18:32:44 -0300

    nav header menu ready and with responsiveness

---
+++ script.js
@@ -0,0 +1,32 @@
+/*Nav Menu início*/
+
+const menuMobile=document.getElementById('menuMobileId');
+const searchIcon=document.getElementById('searchInputIconMobile');
+
+searchIcon.addEventListener('click',e=>{
+    const searchField=document.getElementById('inputSearchFieldId');
+    const logoMobile=document.getElementById('logoMenuId');
+
+    if (searchField.classList.contains('hide')){
+        searchField.classList.remove('hide');
+        logoMobile.classList.add('hide');
+    } else {
+        searchField.classList.add('hide');
+        logoMobile.classList.remove('hide');  
+    }
+});
+
+menuMobile.addEventListener('click',e=>{
+    const menuMobileLinks=document.getElementById('linksMenuMobileId');
+    
+    if(menuMobile.classList.contains('open')){
+        menuMobile.classList.remove('open');
+        menuMobileLinks.classList.add('hide');
+    } else {
+        menuMobile.classList.add('open');
+        menuMobileLinks.classList.remove('hide');
+    }
+})
+
+/* Nav Menu fim*/ 
+
